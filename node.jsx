f9752ab357c162f5292d4fd2d3eccd7e1913f200
Ashish Dixit <163759660+Ashish123897@users.noreply.github.com>
2024-10-12 12:51:02 +0530

    Create node.jsx

---
+++ node.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function show() {
+  return (
+    <div>
+      <p> Hello my name is Ashish </p>
+      <p>I am learning React </p>
+    </div>
+  );
+}
+export default show;
