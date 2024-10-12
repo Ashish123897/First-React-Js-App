dad4086102721a2003156fdd5d7ad32757f51494
Ashish Dixit <163759660+Ashish123897@users.noreply.github.com>
2024-10-12 12:50:11 +0530

    Create image.jsx

---
+++ image.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const source =
+  "https://miro.medium.com/v2/resize:fit:1400/1*kxBdslclglg4zgCw0NMIIA.png";
+function imgica() {
+  return source;
+}
+export default imgica;
